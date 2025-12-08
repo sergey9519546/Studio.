c9e8cee29d3164beaa1158b371153aaa44176855
sergey9519 <sergeyavetisyan1995@gmail.com>
2025-12-08 12:48:18 -0800

    commit

--- prisma/prisma.config.ts
+++ prisma/prisma.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from "@prisma/config";
+import 'dotenv/config'
+import { defineConfig, env } from 'prisma/config'
 
 export default defineConfig({
-  datasource: {
-    url: process.env.DATABASE_URL || "",
+  schema: 'prisma/schema.prisma',
+  migrations: {
+    path: 'prisma/migrations',
   },
-});
+  datasource: {
+    url: env('DATABASE_URL'),
+  },
+})
